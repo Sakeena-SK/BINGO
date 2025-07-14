e4fbc488d5e0530314d1d6b907d1c94526916645
Sakeena-SK <sakeenas.kadhem@gmail.com>
2025-07-14 10:20:12 +0300

    Sakeena project 14th july

--- js/app.js
+++ js/app.js
@@ -16,3 +16,68 @@
 // -If a player wins display 'player wins'
 // -optiions to 'play again' or 'restart the game' 
 
+const homePage = document.querySelector('.Homepage')
+const page1 = document.querySelector('.page1')
+const playBtn = document.querySelector('.Play')
+const solo = document.querySelector('.solo')
+const compete = document.querySelector('.compete')
+const playBoard1 = document.querySelector('.playboard1')
+const playBoard2 = document.querySelector('.playboard2')
+const reset = document.querySelector('.reset')
+const squares = document.querySelectorAll('.board1 .sqr')
+// const Board2 = document.querySelector('.board2')
+
+
+playBtn.addEventListener('click', () => {
+    homePage.style.display = 'none'
+    page1.style.display = 'block'
+    
+})
+
+solo.addEventListener('click', () => {
+    page1.style.display = 'none'
+    playBoard1.style.display = 'block'
+    
+})
+
+compete.addEventListener('click', () => {
+    page1.style.display = 'none'
+    playBoard2.style.display = 'flex'
+    
+})
+
+playBoard2.addEventListener('click', () => {
+    playBoard2.style.display = 'none'
+    homePage.style.display = 'block'
+    
+})
+
+playBoard1.addEventListener('click', () => {
+    playBoard1.style.display = 'none'
+    homePage.style.display = 'block'
+    
+})
+
+const number = []
+const randomIntBoard = (board1) => {
+number.length = 0
+
+    for (i = 1; i<=25; i++) {
+        number.push(i)
+
+    }
+}
+
+const arrayBoard = (array) => {
+    for (i = array.length - 1; i > 0; i--) {
+        num = Math.floor(Math.random() * (i + 1))
+    }
+arrayBoard(number)
+
+squares.forEach((square, index) => {
+    square.textContent = number[index]
+})
+}
+
+
+
